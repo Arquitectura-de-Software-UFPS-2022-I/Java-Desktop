ad5624e9e38be90fec410d0ed455ba5aea8c341f
OMAR RAMON <omarmontes.879@gmail.com>
2022-03-08 17:58:13 -0500

    Form Signature requests

---
+++ src/Vista/SignatureRequestModel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import models.SignatureRequestDto;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class SignatureRequestModel extends AbstractTableModel {
+
+    protected List<SignatureRequestDto> requests;
+
+    private final String[] columnNames = {"Options", "Subject", "Create Date"};
+    private final Class[] columnClasses = {String.class, String.class, String.class};
+
+    public SignatureRequestModel(List<SignatureRequestDto> requests) {
+        this.requests = requests;
+    }
+
+    @Override
+    public int getRowCount() {
+        return requests.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        SignatureRequestDto usuario = this.requests.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return "";
+            case 1:
+                return usuario.getSubject();
+            case 2:
+                return usuario.getCreate_date();
+        }
+        return ("");
+
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columnNames[columnIndex];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return columnClasses[columnIndex];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+    
+}
---
+++ src/utils/ComboItem.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ComboItem {
+
+    private int key;
+    private String value;
+
+    public ComboItem(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+}
